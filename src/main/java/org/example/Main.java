937933984305291abfc375bf1e6e803167cbf205
Venaktasukumar <malleboina.sukumar@tringapps.com>
2023-01-27 18:47:39 +0530

    firest commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,42 @@
 package org.example;
 import java.util.Scanner;
+import java.util.logging.*;
 class Account{
     String accountholdersname;
     private final long accountnumber;
     private double balance;
     public Account(String name, long accnumber, long baalance){
+        Logger l= Logger.getLogger("com.api.jar");
         accountholdersname=name;
         accountnumber=accnumber;
         balance=baalance;
     }
     public void deposit(long deposit){
-        System.out.println(accountholdersname);
-        System.out.println(accountnumber);
-        System.out.println("Before Depositing balance"+" "+balance);
+        Logger l= Logger.getLogger("com.api.jar");
+        l.info(accountholdersname);
+        l.info(String.valueOf(accountnumber));
+        l.info("Before Depositing balance"+" "+balance);
         balance=balance+deposit;
-        System.out.println("After Depositing Balance"+" "+balance);
+        l.info("After Depositing Balance"+" "+balance);
     }
     public void withdrawal(long withdrwalamount){
-        System.out.println(accountholdersname);
-        System.out.println(accountnumber);
-        System.out.println("Before Withdrawal balance"+" "+balance);
+        Logger l= Logger.getLogger("com.api.jar");
+        l.info(accountholdersname);
+        l.info(String.valueOf(accountnumber));
+        l.info("Before Withdrawal balance"+" "+balance);
         balance=balance-withdrwalamount;
-        System.out.println("After Withdrawal Balance"+" "+balance);
+        l.info("After Withdrawal Balance"+" "+balance);
     }
     public void accbalance(){
-        System.out.println(accountholdersname);
-        System.out.println(accountnumber);
-        System.out.println("Account Balance"+" "+balance);
+        Logger l= Logger.getLogger("com.api.jar");
+        l.info(accountholdersname);
+        l.info(String.valueOf(accountnumber));
+        l.info("Account Balance"+" "+balance);
     }
 
 
     public static void main(String[] args){
+        Logger l= Logger.getLogger("com.api.jar");
         Scanner sc=new Scanner(System.in);
         System.out.print("Account Holders Name:");
         String name=sc.nextLine();
@@ -40,19 +46,19 @@ class Account{
         long balance=sc.nextLong();
         Account acc=new Account(name,accnumber,balance);
         while(true){
-            System.out.println("1.Deposit");
-            System.out.println("2.withdrwal");
-            System.out.println("3.Balance");
-            System.out.println("4.Exit");
-            System.out.println("Enter Your Choice");
+            l.info("1.Deposit");
+            l.info("2.withdrwal");
+            l.info("3.Balance");
+            l.info("4.Exit");
+            l.info("Enter Your Choice");
             int a=sc.nextInt();
             if(a==1){
-                System.out.println("Enter the amount to be deposit");
+                l.info("Enter the amount to be deposit");
                 long deeposit=sc.nextLong();
                 acc.deposit(deeposit);
             }
             else if(a==2){
-                System.out.println("Enter the amount to be withdrawal");
+                l.info("Enter the amount to be withdrawal");
                 long withdrawalAmount=sc.nextLong();
                 acc.withdrawal(withdrawalAmount);
             }
@@ -60,7 +66,7 @@ class Account{
                 acc.accbalance();
             }
             else{
-                System.out.println("Exit");
+                l.info("Exit");
                 break;
             }
         }
