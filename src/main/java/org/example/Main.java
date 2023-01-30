6fa5bbf5e8e3a766f8df29fb6af091ca0e4c50df
Venaktasukumar <malleboina.sukumar@tringapps.com>
2023-01-30 18:13:40 +0530

    firest commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,5 @@
 package org.example;
-import java.util.Scanner;
+import java.util.*;
 import java.util.logging.*;
 class Account{
     Logger l= Logger.getLogger("com.api.jar");
@@ -45,39 +45,44 @@ class Account{
     public static void main(String[] args){
         Logger l= Logger.getLogger("com.api.jar");
         Scanner sc=new Scanner(System.in);
-        l.info("Account Holders Name:");
-        String name=sc.nextLine();
-        l.info("Account Number:");
-        long accnumber=sc.nextLong();
-        l.info("Account Holder Balance:");
-        long balance=sc.nextLong();
-        Account acc=new Account(name,accnumber,balance);
-        while(true){
-            l.info("1.Deposit");
-            l.info("2.withdrwal");
-            l.info("3.Balance");
-            l.info("4.Exit");
-            l.info("Enter Your Choice");
-            int a=sc.nextInt();
-            if(a==1){
-                l.info("Enter the amount to be deposit");
-                long deeposit=sc.nextLong();
-                acc.deposit(deeposit);
+        try {
+            l.info("Account Holders Name:");
+            String name = sc.nextLine();
+            l.info("Account Number:");
+            long accnumber = sc.nextLong();
+            l.info("Account Holder Balance:");
+            long balance = sc.nextLong();
+            Account acc = new Account(name, accnumber, balance);
+            while (true) {
+                l.info("1.Deposit");
+                l.info("2.withdrwal");
+                l.info("3.Balance");
+                l.info("4.Exit");
+                l.info("Enter Your Choice");
+                int a = sc.nextInt();
+                if (a == 1) {
+                    l.info("Enter the amount to be deposit");
+                    long deeposit = sc.nextLong();
+                    acc.deposit(deeposit);
+                } else if (a == 2) {
+                    l.info("Enter the amount to be withdrawal");
+                    long withdrawalAmount = sc.nextLong();
+                    acc.withdrawal(withdrawalAmount);
+                } else if (a == 3) {
+                    acc.accbalance();
+                } else if(a==4){
+                    l.info("Exit");
+                    break;
+                }
+                else{
+                    l.info("invalid choice");
+                }
             }
-            else if(a==2){
-                l.info("Enter the amount to be withdrawal");
-                long withdrawalAmount=sc.nextLong();
-                acc.withdrawal(withdrawalAmount);
-            }
-            else if(a==3){
-                acc.accbalance();
-            }
-            else{
-                l.info("Exit");
-                break;
-            }
-        }
 
-        sc.close();
+            sc.close();
+        }
+        catch(InputMismatchException e){
+            l.info("invalid input "+e);
+        }
     }
 }
